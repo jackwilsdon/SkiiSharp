8897a2dd7db2d1fc2a16fe4988e1819d97ab57ac
Jack Wilsdon <jack.wilsdon2@gmail.com>
2015-08-22 00:13:12 +0100

    Add basic C# console project

---
+++ skii-sharp/Program.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace skiisharp
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}

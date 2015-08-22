9faf3ba09876988dd1bb8f39d25b8a6b261e0b2f
Jack Wilsdon <jack.wilsdon2@gmail.com>
2015-08-22 02:42:27 +0100

    Use logger to output text

--- SkiiSharp/Program.cs
+++ SkiiSharp/Program.cs
@@ -1,9 +1,15 @@
 using System;
 
+using Log;
+using Log.Destinations;
+
 namespace SkiiSharp {
     class MainClass {
         public static void Main(string[] args) {
-            Console.WriteLine("Hello World!");
+            Logger logger = new Logger();
+
+            logger.AddDestination(new ConsoleDestination());
+            logger.Log("Hello!");
         }
     }
 }

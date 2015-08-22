20a69245964c935979b61c844e2d39718c345307
Jack Wilsdon <jack.wilsdon2@gmail.com>
2015-08-22 02:18:20 +0100

    Add Parse method to LogMessage to parse objects into an array of messages

--- Log/LogMessage.cs
+++ Log/LogMessage.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections.Generic;
 
 namespace Log {
     public class LogMessage {
@@ -12,6 +13,31 @@ namespace Log {
         public LogMessage(LogColor color, String value) {
             Color = color;
             Value = value;
+        }
+
+        public static LogMessage[] Parse(params Object[] messages) {
+            List<LogMessage> parsedMessages = new List<LogMessage>();
+
+            LogMessage currentMessage = new LogMessage();
+
+            foreach (Object message in messages) {
+                Type messageType = message.GetType();
+
+                if (messageType == typeof(LogMessage)) {
+                    parsedMessages.Add((LogMessage) message);
+                } else if (messageType == typeof(LogMessage[])) {
+                    parsedMessages.AddRange((LogMessage[]) message);
+                } else if (messageType == typeof(LogColor)) {
+                    currentMessage.Color = (LogColor) message;
+                } else {
+                    currentMessage.Value += message.ToString();
+
+                    parsedMessages.Add(currentMessage);
+                    currentMessage = new LogMessage();
+                }
+            }
+
+            return parsedMessages.ToArray();
         }
     }
 }

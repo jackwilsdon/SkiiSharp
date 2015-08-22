e4a21be9dda7257777c9c176b9461efb14a2f393
Jack Wilsdon <jack.wilsdon2@gmail.com>
2015-08-22 02:18:38 +0100

    Add body of Logger class

--- Log/Logger.cs
+++ Log/Logger.cs
@@ -1,7 +1,38 @@
 using System;
+using System.Collections.Generic;
 
 namespace Log {
     public class Logger {
+        private HashSet<ILogDestination> destinations = new HashSet<ILogDestination>();
 
+        public LogLevel Level = LogLevel.Debug;
+
+        public bool AddDestination(ILogDestination destination) {
+            return destinations.Add(destination);
+        }
+
+        public bool RemoveDestination(ILogDestination destination) {
+            return destinations.Remove(destination);
+        }
+
+        public void Log(LogLevel level, params Object[] messages) {
+            if (level < Level) {
+                return;
+            }
+
+            if (destinations.Count == 0) {
+                throw new InvalidOperationException("no destinations for logger");
+            }
+
+            LogMessage[] logMessages = LogMessage.Parse(messages);
+
+            foreach (ILogDestination destination in destinations) {
+                destination.Log(level, logMessages);
+            }
+        }
+
+        public void Log(params Object[] messages) {
+            Log(LogLevel.Information, messages);
+        }
     }
 }

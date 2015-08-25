e3380d7cec0333c712a7398604c3c6fb5bb4a28f
Jack Wilsdon <jack.wilsdon2@gmail.com>
2015-08-25 17:35:43 +0100

    Replace indentation with spaces

--- ECS/MyClass.cs
+++ ECS/MyClass.cs
@@ -2,11 +2,11 @@ using System;
 
 namespace ECS
 {
-	public class MyClass
-	{
-		public MyClass ()
-		{
-		}
-	}
+    public class MyClass
+    {
+        public MyClass ()
+        {
+        }
+    }
 }
 
